4770438177838cf8cf15d894db5e309d0ccccb39
jltu <jltu@hawaii.edu>
2018-02-19 23:54:21 -1000

    Tested and Success

--- lab3/problem/src/List.cpp
+++ lab3/problem/src/List.cpp
@@ -1,118 +1,136 @@
 #include "List.hpp"
+
 #include <utility>
 
-//Default Constructor
-List::List():
-		head(nullptr);
-		length(0) {}
+// Initializes the list
+List::List() :
+  head(nullptr),
+  length(0) {}
 
-		//Copy Constructor
-		List::List(const List& other){
-				ListNode* otherhead = other.head;
-				length = other.length;
-				while(otherhead != nullptr){
-						otherhead = other.head->next;
-				}
-		}
+// Performs a deep copy, copying each node of the other list
+// into this one
+List::List(const List& other){
+  ListNode* otherHead = other.head;
+  length = other.length;
+  while (otherHead != nullptr){
+    otherHead = otherHead->next;
+    }
+  }
 
-//Move Constructor
-List::List(List&& other){
-		head(std::move(other.head));
-		lenght(std::move(other.length)){
-				other.head = nullptr;
-				other.length = 0;
-		}
+// Performs a move, moving the internal list from the other
+// vector to this one, and invalidates the other list pointer
+// for its memory
+List::List(List&& other) :
+  head(std::move(other.head)),
+  length(std::move(other.length)){
+  other.head = nullptr;
+  other.length = 0;
+  }
+
+// Deallocates the memory in this list.
+List::~List(){
+  this->head = nullptr;
 }
 
-//Destructor
-List::~List(void){
-		this->head = nullptr;
+// For all of the following functions,
+// throw a const char* (string) if the index is out of
+// the bounds of the list.
 
-}
-
-//Append
+// Appends a ListNode onto the end of the list
+//
+// Throw a const char* if the index is out of bounds
 void List::append(int num){
-		//Case 1: List is empty
-		if(this->head == nullptr){        	//if list is empty
-				this->head = new ListNode(num);
-		}
-		//Case 2: List not empty so append to end
-		else{
-				ListNode *it = this->head;        //creates a pointer named "it" that points to head
-				while(it->next != nullptr){       //while the next node is not the tail
-						it = it->next;                //iterate it down the list until
-				}                                 //the next node becomes the tail.
-				it->next = new ListNode(num);     //create a node and make "it" point to node
-		}
+  //if appending to an empty list
+  if(this->head == nullptr){
+    this->head = new ListNode(num);
+  }
+  //otherwise appending to an existing node
+  else {
+    ListNode *it = this->head;
+    while(it->next != nullptr){
+      it = it->next;
+    }
+    it->next = new ListNode(num);
+  }
+  length++;
 }
 
-//Insert
+// Inserts a ListNode before the index
+//
+// Throw a const char* if the index is out of bounds.
+// Appending at the end is valid (e.g. insert(list.size(), 0)
+// is valid)
 void List::insert(int index, int num){
-		//Guard to check if index is valid
-		if(index > length || index < 0){
-				throw "List Index is not in scope"
-		}
-		//Case 1: if inserting to an empty list
-		else if(this->head == nullptr){
-				this->head = new ListNode(num);
-		}
-		else{
-			ListNode *temp = this->head;
-			for(int i = 1; i != index; i++){
-				temp = temp->next;
-			}
-			temp->next = new ListNode(num);
-		}
-		length++;
+  if(index < 0 || index > length){
+    throw "List.insert: index out of bounds";
+  }
+  //if inserting to an empty list
+  if(this->head == nullptr){
+    this->head = new ListNode(num);
+  }
+  //either insert in middle of list or end
+  else{
+    ListNode* temp = this->head;
+    for(int i = 1; i != index; i++){
+      temp = temp->next;
+    }
+    temp->next = new ListNode(num);
+  }
+  length++;
 }
-//Remove
+
+// Removes the ListNode at the index
+//
+// Throw a const char* if the index is out of bounds.
 void List::remove(int index){
-		//Guard to check if index is valid
-		if(index < 0 || index > length ||this-head == nullptr){
-				throw "List.remove: index out of bounds";
-		}
-		ListNode *it = this->head;
-		for(int i = 0; i != index; i++){
-			it = it->next;
-		}
-		delete it;
-		length--;
-		this->head = it->next;
+  if(index < 0 || index > length || this->head == nullptr){
+    throw "List.remove: index out of bounds";
+  }
+  ListNode *it = this->head;
+  for(int i = 0; i != index; i++){
+    it = it->next;
+  }
+  delete it;
+  length--;
+  this->head = it->next;
 }
 
-//get
-void List::int get(int index) const{
-		if (index < 0 || index > length){
-				throw "List.insert: index out of bounds";
-		}
-		ListNode *c = head;
-		int count = 0;
-		while(c != nullptr){
-				if(count == index){
-						return(c->data);
-				}
-				c = c->next;
-				count++;
-		}
+// Returns the int at the index
+//
+// Throw a const char* if the index is out of bounds.
+int List::get(int index) const{
+  if(index < 0 || index > length){
+    throw "List.insert: index out of bounds";
+  }
+  ListNode *c = head;
+  int count = 0;
+  while(c != nullptr){
+    if(count == index){
+      return(c->data);
+    }
+    c = c->next;
+    count++;
+  }
 }
 
-//Returns a readable/writeabale referece to the element at index
-std::size_t size() const {returns length;}
+// Returns the length of the list
+std::size_t List::size() const{ return length;}
 
-//Overloaded operator
-List::int& operator[](int index){
-		if(index < 0 || index > length){
-				throw "List.operator: index out of bounds";
-		}
-		ListNode *c = head;
-		int count = 0;
-		while(c != nullptr){
-				if(count == index){
-						int& ref = c->data;
-						return (ref);
-				}
-				c = c->next;
-				count++;
-		}
+// Reutrns a readable/writeable reference to the element at index
+//
+// Throw a const char* if the index is out of bounds.
+int& List::operator[](int index){
+  if(index < 0 || index > length){
+    throw "List.operator: index out of bounds";
+  }
+  ListNode *c = head;
+  int count = 0;
+  while(c != nullptr){
+    if(count == index){
+      int& ref = c->data;
+      return(ref);
+    }
+    c = c->next;
+    count++;
+  }
 }
-
--- lab3/problem/src/Vector.cpp
+++ lab3/problem/src/Vector.cpp
@@ -2,105 +2,98 @@
 #include <utility>
 
 //Default Constructor
-Vector::Vector():
-		arr(new int[1]),
-		length(0),
-		capacity(1)	{}
-		
+Vector::Vector() :
+  arr(new int[1]),
+  length(0),
+  capacity(1) {}
 
 //Copy Constructor
-Vector::Vector(const Vector &other) :
-		arr(new int[other.capacity]),
-		length(other.size()),
-		capacity(other.capacity)
-{ for(std::size_t i=0; i<other.size(); i++){
-		this->arr[i] = other.arr[i];
-										   }
-}
+Vector::Vector(const Vector& other) :
+  arr(new int [other.size()]),
+  length(other.size()),
+  capacity(other.capacity){
+    for (std::size_t i=0; i<other.size(); i++){
+      this->arr[i] = other.arr[i];
+    }
+  }
 
 //Move Constructor
-Vector::Vector(Vector&& other) :
-		arr(other.arr);
-		length(other.length);
-		capacity(other.capacity){
-				arr[i] = nullptr;
-		}
+Vector::Vector(Vector&& other){
+  arr = other.arr;
+  length = other.length;
+  other.arr = nullptr;
+  other.length = 0;
+}
 
-//Deallocate and delete the memory allocated earlier
+//Destructor that deallocates memory
 Vector::~Vector(void){
-		delete arr[];
+  delete [] arr;
 }
 
-//Appends a number to the end of the vector
-void append(int num){
-		if((length+1) > capacity) {double_capacity();}
-		arr[length] = num;
-		length++;
+//appends a number to the end of the Vector
+void Vector::append(int num){
+    if(length+1 > capacity){ double_capacity(); }
+    arr[length] = num;
+    length++;
 }
 
-//Inserts a new number before the index
+//inserts a new number before the index
 void Vector::insert(int index, int num){
-		//Check index out of bounds
-		if(index < 0 || index > length){
-				throw "Vector.double_capacity: Index Error"
-		}
-		//check to ensure there is enough capacity
-		if((this->length+1) >= this->capacity) {this->double_capacity();}
-		//Iterate to index
-		for(std::size_t i = length;i > index ;i--;){
-				arr[i+1] = arr[i];
-		}
-		//Once at index, insert
-		arr[index] = num;
-		//Increment value
-		length++;
+  if(index < 0 || index > length){
+    throw "Vector.insert: index out of bounds";
+  }
+  if(this->length +1 >= this->capacity){
+    this->double_capacity();
+  }
+  for(int i = this->length; i > index; --i){
+    arr[i] = arr[i-1];
+  }
+  arr[index] = num;
+  length++;
 }
 
-void Vector::double_capacity(){
-		//Allocate memory with double current capacity
-		int *temp = new int[2*capacity];
-		//Store new size of capacity
-		int temp_capacity = 2*capacity;
-		//Iterate through old vector and copy values
-		for(std::size_t i = 0; this->length; i++){
-				temp[i] = arr[i];
-		}
-		//Delete old Vector
-		delete arr[];
-		//copy new vector into old vector
-		this->arr = temp;
-		//copy new capacity to old capacity
-		this->capacity = temp_capacity;
-}
-
-//Remove
+// Removes a number at the index
 void Vector::remove(int index){
-		if(vector < 0 || index >= length){
-			throw "Vector.remove: index out of bounds"; 
-		}
-		else{
-			for(; index <length; index++)
-				arr[index] = arr[index+1];
-			arr[length-1] = 0;
-			length--;
-		}
+  if(index < 0 || index >= length){
+    throw "Vector.remove: index out of bounds";
+  }
+  else{
+    for (; index<length; index++)
+      arr[index] = arr[index+1];
+    arr[length-1] = 0;
+    length--;
+  }
 }
-//Gets a number at the index
+
+// Gets a number at the index
 int Vector::get(int index) const{
-		if(index < 0 || index > length){
-			throw "Vector.get: index out of bounds";
-		}
-		return arr[index];
-
-std::size_t size() const {returns length;}
-
-int& operator[](int index){
-		if(index > 0 || index > length){
-			throw "Vector.operator: index out of bounds";
-		}
-		int& ref = arr[index];
-		return ref;
+  if(index < 0 || index > length){
+    throw "Vector.get: index out of bounds";
+  }
+  return arr[index];
 }
 
+//double_capacity
+void Vector::double_capacity(){
+  int *temp = new int[2*capacity];
+  std::size_t temp_capacity = 2*capacity;
+  for(std::size_t i=0; i < this->length; i++){
+    temp[i] = arr[i];
+  }
+  delete [] arr;
+  this->arr = temp;
+  this->capacity = temp_capacity;
+}
 
+// Returns the length of the vector
+std::size_t Vector::size() const{ return length; }
 
+// Returns a readable/writable referece to an element
+// Throw a const char* if the index is out of bounds
+int& Vector::operator[](int index){
+  if(index < 0 || index > length){
+    throw "Vector.operator: index out of bounds";
+  }
+  int& ref = arr[index];
+  return ref;
+}
